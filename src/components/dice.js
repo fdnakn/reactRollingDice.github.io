396b3c1d2a045283f32b1e9a5538c4743acc4ac4
akin <akinfidancal@hotmail.com>
2022-08-03 20:01:46 +0300

    reactrolldice

---
+++ src/components/dice.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './dice.css'
+
+function dice({face ,rolling}) {
+  return (
+    <i className={`dice fas fa-dice-${face} ${rolling && "shaking"}`}></i>
+  )
+}
+
+export default dice
